d9e747fbdc6aa489b404bf50d80c224bcaea0446
Luigi Gargioni <luigi.gargioni@gmail.com>
2023-07-04 12:02:43 +0200

    enabled manager routes

--- frontend/src/routes/index.ts
+++ frontend/src/routes/index.ts
@@ -1,22 +1,20 @@
 import { useRoutes } from 'react-router-dom'
 
-/* import { getFromLocalStorage, LocalStorageKey } from 'utils/localStorageUtils'
-import { USER_GROUP } from 'utils/constants' */
+import { getFromLocalStorage, LocalStorageKey } from 'utils/localStorageUtils'
+import { USER_GROUP } from 'utils/constants'
 import { ManagerRoutes } from './AdminRoutes'
 import { UserRoutes } from './UserRoutes'
 import { AuthRoutes } from './AuthRoutes'
 import { MainRoutes } from './MainRoutes'
 
 export const Routes = () => {
-  // const { group } = getFromLocalStorage(LocalStorageKey.USER)
+  const { group } = getFromLocalStorage(LocalStorageKey.USER)
 
   const defaultRoutes = [MainRoutes, AuthRoutes, UserRoutes]
-  /*   const routes =
+  const routes =
     group === USER_GROUP.MANAGER
       ? [...defaultRoutes, ManagerRoutes]
-      : defaultRoutes */
-
-  const routes = [...defaultRoutes, ManagerRoutes]
+      : defaultRoutes 
 
   return useRoutes(routes)
 }
